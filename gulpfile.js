ea70acd933ba1a245496147f5c24dd76a683da87
turnerlc <turnerlc@gmail.com>
2014-10-07 16:39:50 -0400

    Modified GULP

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,17 @@ var  gulp = require('gulp'),
     gutil = require('gulp-util'),
    coffee = require('gulp-coffee'),
    browserify = require('gulp-browserify'),
+  compass = require('gulp-compass'),
    concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSource      = ['components/scripts/rclick.js',
 					 'components/scripts/pixgrid.js',
 				     'components/scripts/tagline.js',
-				     'components/scripts/template.js'
-				     
-				     ];
+				     'components/scripts/template.js'				     
+				     ];				     
+
+var sassSources  = ['components/sass/style.scss'];				     
 
 gulp.task('coffee',function(){
 
@@ -30,3 +32,29 @@ gulp.task('js', function(){
         .pipe(gulp.dest('builds/development/js'))
 
 });
+
+gulp.task('compass', function(){
+
+    gulp.src(sassSources)
+        .pipe(compass({
+ 			  sass:   'components/sass',
+			  images: 'builds/development/images',
+			  style:  'expanded'       
+        }))
+           .on('error', gutil.log)  
+        .pipe(gulp.dest('builds/development/css'))
+
+});
+
+
+gulp.task('watch', function(){
+
+  gulp.watch(coffeeSources,['coffee']);
+  gulp.watch(jsSource,['js']);
+  //gulp.watch('components/sass/*.scss',['compass']);
+
+});
+
+
+gulp.task('default', ['coffee','js','watch']);
+

8da9264a0749ff8217df510720b1d8694acfa621
turnerlc <turnerlc@gmail.com>
2014-10-07 13:45:55 -0400

    Added Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var  gulp = require('gulp'),
     gutil = require('gulp-util'),
    coffee = require('gulp-coffee'),
+   browserify = require('gulp-browserify'),
    concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
-var jsSource     = ['components/scripts/rclick.js',
+var jsSource      = ['components/scripts/rclick.js',
 					 'components/scripts/pixgrid.js',
 				     'components/scripts/tagline.js',
 				     'components/scripts/template.js'
@@ -25,6 +26,7 @@ gulp.task('js', function(){
 
     gulp.src(jsSource)
         .pipe(concat('script.js'))
+        .pipe(browserify())
         .pipe(gulp.dest('builds/development/js'))
 
 });
